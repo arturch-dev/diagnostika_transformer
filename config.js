1d776cede1169656431be638b5f0df0d4ae3b87b
YuransiS <yura3zaxar@outlook.com>
2026-04-24 16:04:24 +0300

    feat: add WayForPay integration via Google Apps Script and implement lead collection form with UTM tracking

---
+++ config.js
@@ -0,0 +1,5 @@
+const APP_CONFIG = {
+    // URL вашого Google Apps Script. 
+    // Це єдиний шлях до "бекенду", який має знати фронтенд.
+    API_ENDPOINT: 'https://script.google.com/macros/s/AKfycbxsHZ7XHi70M33guhCc_S3tOYazucBBb4oAHCN0FQsXIVK9nizQRHgf-yFYHsdxuooAbQ/exec'
+};
